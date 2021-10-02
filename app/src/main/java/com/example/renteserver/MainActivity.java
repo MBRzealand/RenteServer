9d687eda4b347aa6a6914bb06e60cff2c4c53ac9
MBRzealand <mikk24f6@edu.easj.dk>
2021-10-02 12:02:05 +0200

    made textview scrollable and fixed a ton of bugs

--- app/src/main/java/com/example/renteserver/MainActivity.java
+++ app/src/main/java/com/example/renteserver/MainActivity.java
@@ -6,10 +6,12 @@ import static androidx.core.content.ContentProviderCompat.requireContext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.text.format.Formatter;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
@@ -25,7 +27,7 @@ import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
-    static int port = 7890;
+    static int port = 6969;
     TextView output;
     Button button;
     private ServerSocket serverSocket;
@@ -46,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
         output = findViewById(R.id.output);
         button = findViewById(R.id.button);
 
+        output.setMovementMethod(new ScrollingMovementMethod());
+
     }
 
     @Override
@@ -58,10 +62,21 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
+        button.setClickable(true);
+        button.setBackgroundColor(getResources().getColor(R.color.purple_200));
     }
 
 
     Runnable connect = () -> {
+
+        if (serverSocket != null) {
+            try {
+                serverSocket.close();
+                connected = false;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         try {
 
@@ -69,16 +84,16 @@ public class MainActivity extends AppCompatActivity {
 
             serverSocket = new ServerSocket(port);
 
-            output.append("\nAccepting connection on port " + port + ".");
+            output.append("\n" + "Accepting connection on port " + port + ".");
 
 
 
-            while (true) {
+            while (!Thread.currentThread().isInterrupted()) {
 
                 socket = serverSocket.accept();
 
                 if(!connected){
-                    output.append("\nConnection established from: " + socket.getRemoteSocketAddress().toString());
+                    output.append("\n" + "Connection established from: " + socket.getRemoteSocketAddress().toString());
                     output.append("\n");
                     connected = true;
                 }
@@ -100,13 +115,17 @@ public class MainActivity extends AppCompatActivity {
 
 
         } catch (Exception e) {
-            output.append(e.getMessage());
+            output.append("\n" + e.getMessage());
+            button.setClickable(true);
+            button.setBackgroundColor(getResources().getColor(R.color.purple_200));
         }
+
     };
 
     public void startServer(View view) {
 
-//        button.setVisibility(View.GONE);
+        button.setClickable(false);
+        button.setBackgroundColor(Color.GRAY);
 
 
         WifiManager wifiMgr = (WifiManager) getSystemService(WIFI_SERVICE);
@@ -125,21 +144,21 @@ public class MainActivity extends AppCompatActivity {
         switch(counter) {
             case 1:
                 annualIntrestRate = inputString;
-                output.append("\nAnnual interest rate: " + annualIntrestRate);
+                output.append("\n" + "Annual interest rate: " + annualIntrestRate);
             break;
 
             case 2:
                 numberOfYears = inputString;
-                output.append("\nNumber of years: " + numberOfYears);
+                output.append("\n" + "Number of years: " + numberOfYears);
             break;
 
             case 3:
                 loanAmount = inputString;
-                output.append("\nLoan amount: " + loanAmount);
+                output.append("\n" + "Loan amount: " + loanAmount);
 
 
                 calculatedLoan = String.valueOf(Double.parseDouble(loanAmount)*(Math.pow((1.00+Double.parseDouble(annualIntrestRate)),Double.parseDouble(numberOfYears))));
-                output.append("\ncalculation: " + calculatedLoan);
+                output.append("\n" + "calculation: " + calculatedLoan);
             break;
 
         }
